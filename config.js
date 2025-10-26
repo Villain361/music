b373481b195d6acd3d4dbb9be5872fe12e827d99
Villain361 <freegamer758808@gmail.com>
2025-10-26 16:44:29 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  TOKEN: "MTM0MTI2NzU2MzY3NzYxODI4OA.G7ULBe.li4kMrFaPwRTGH-zaMKAywDeNT4_hWN90hzMA0", //(F Y)MTQzMTIzODg2MjQ2NTc5NDE1Mg.G_7VBV.I-RYDymnoHdVGMj0IV0jpJU_chbPn8x3N3jHXI //(sphere)MTM0MTI2NzU2MzY3NzYxODI4OA.G7ULBe.li4kMrFaPwRTGH-zaMKAywDeNT4_hWN90hzMA0
+  language: "en",
+  ownerID: ["1238074550097543201", ""], 
+  mongodbUri : "mongodb+srv://musicbot:FahadH0458E@cluster0.fhmnnjg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  spotifyClientId : "65f5b9d811414706ac079a356eaaefa3",
+  spotifyClientSecret : "ec8650f27244436095551d2dda833c29",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "YouTube Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/y9tUcrb536",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+     {
+      name: "Hunter",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 80,
+      secure: false
+    }
+  ]
+}
